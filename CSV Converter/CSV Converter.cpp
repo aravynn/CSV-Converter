e80cc6d4254aa9162869b320d80cb13d952e4c9e
Kevin Jones <aravynn@gmail.com>
2021-04-21 15:54:01 -0500

    CSV Converter Initial

---
+++ CSV Converter/CSV Converter.cpp
@@ -0,0 +1,15 @@
+// CSV Converter.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main()
+{
+    std::cout << "CSV Generator\n";
+    
+    // application will use 2 existing CSV's 
+
+}
